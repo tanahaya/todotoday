67492a1d4a3a1312fd1a47ab42ce590785bb6f72
tanahaya <tanakachihiro@tanakachihiro-no-macbook-air.local>
2016-05-31 18:54:03 +0900

    Initial Commit

---
+++ todotoday/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  todotoday
+//
+//  Created by 田中千洋 on 2016/05/31.
+//  Copyright © 2016年 田中 颯. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
